1d72981ae4d3088c6cb05bdb062ce74416344df5
Lauryne Sutur <suturlauryne@gmail.com>
2022-12-05 09:56:16 +0100

    first commit

---
+++ menu.js
@@ -0,0 +1,14 @@
+// Création des variables
+const menuHamburger = document.querySelector(".menu-hamburger") // Indiquer la class de l'image.
+const navLinks = document.querySelector(".nav-links") // Indiquer la class des boutons de la nav.
+const titre = document.querySelector(".titre")
+const paragraphe = document.querySelector(".paragraphe")
+
+// A chaque fois qu'on clique sur la variable menuHamburger, la class "mobile-menu" s'ajoute a la classe nav-links et inversement
+menuHamburger.addEventListener('click',()=>{
+navLinks.classList.toggle('mobile-menu')
+titre.classList.toggle('titre-menu')
+paragraphe.classList.toggle('paragraphe-menu')
+
+})
+
